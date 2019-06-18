f947cb9a4b545a149181db3da91e956e058683be
Gerardo Pias <gerardo.pias@matesoft.com.uy>
2019-06-18 16:33:33 -0300

    Update scripts.

--- nuevoinstalador.sh
+++ nuevoinstalador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/dbdhn4yd9m3nnct/libcmscodec1_0.0.1-1_amd64.deb
-wget https://www.dropbox.com/s/7c0rs49ewcu6m93/fort_0.0.1-1_amd64.deb
-sudo apt install ./libcmscodec1_0.0.1-1_amd64.deb ./fort_0.0.1-1_amd64.deb -y
+wget https://github.com/NICMx/FORT-validator/releases/download/v0.0.2/fort_0.0.2-1_amd64.deb
+sudo apt install ./fort_0.0.2-1_amd64.deb -y
 
 sudo cp config.json /etc/fort/
+
